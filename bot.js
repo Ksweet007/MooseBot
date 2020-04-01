fde22ba1b085171d9c6c90126c6c95c293e31935
KyleSweetMeM <kyle.sweet@mem.com>
2020-04-01 09:44:39 -0400

    Refactored roller to be a single function

--- bot.js
+++ bot.js
@@ -10,31 +10,39 @@ client.on('ready', () => {
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('message', msg => {    
-    if(msg.content.startsWith("/r")){
+client.on('message', msg => {
+    if (msg.content.startsWith("/r")) {
         //Get indexes for brackets to be able to grab out the value provided
         const indexOfFirstBracket = msg.content.indexOf('[');
         const indexOfSecondBracket = msg.content.indexOf(']');
 
         //Get the Value of what is being rolled without brackets
-        const diceCountWithDiceSize = msg.content.substring(indexOfFirstBracket +1, indexOfSecondBracket);
+        const diceCountWithDiceSize = msg.content.substring(indexOfFirstBracket + 1, indexOfSecondBracket);
 
-        let valueRolled = 0;
-        let numberOfDiceToRoll = 1;
-        let dieSizeToRoll = 0;
-        if(diceCountWithDiceSize.indexOf("d") === 0){
+        let numberOfDiceToRoll;
+        let dieSizeToRoll;
+        if (diceCountWithDiceSize.indexOf("d") === 0) {
             //Rolling a single dice
-            dieSizeToRoll = diceCountWithDiceSize.substring(1)            
-            valueRolled = 1 + Math.floor(Math.random() * dieSizeToRoll);
+            numberOfDiceToRoll = 1;
+            dieSizeToRoll = diceCountWithDiceSize.substring(1)
         }
-        else{
+        else {
             //Rolling multiple dice
             dieSizeToRoll = diceCountWithDiceSize.substring(2);
-            numberOfDiceToRoll = diceCountWithDiceSize.substring(0,diceCountWithDiceSize.indexOf("d"));
-            for(let i =0; i < numberOfDiceToRoll;i++){
-                valueRolled += (1 + Math.floor(Math.random() * dieSizeToRoll))
-            }
+            numberOfDiceToRoll = diceCountWithDiceSize.substring(0, diceCountWithDiceSize.indexOf("d"));
         }
-        msg.reply(`Rolled a ${valueRolled}`)        
+
+        const valueRolled = Roll(numberOfDiceToRoll, dieSizeToRoll);
+        msg.reply(`Rolled a ${valueRolled}`)
     }
-  });
+});
+
+
+function Roll(numberOfDiceToRoll, dieSizeToRoll) {
+    let totalValue = 0;
+    for (let i = 0; i < numberOfDiceToRoll; i++) {
+        totalValue += (1 + Math.floor(Math.random() * dieSizeToRoll))
+    }
+
+    return totalValue;
+}
