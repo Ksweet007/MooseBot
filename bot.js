b65d0326c72c56b7522f74f7cd18c9e439b13a0f
KyleSweetMeM <kyle.sweet@mem.com>
2020-04-01 10:13:35 -0400

    - Added config file - Check prefix and return if it is not found

--- bot.js
+++ bot.js
@@ -1,40 +1,40 @@
 // Run dotenv
-require('dotenv').config();
-
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.login(token);
 
 client.on('message', msg => {
-    if (msg.content.startsWith("/r")) {
-        //Get indexes for brackets to be able to grab out the value provided
-        const indexOfFirstBracket = msg.content.indexOf('[');
-        const indexOfSecondBracket = msg.content.indexOf(']');
+    if (!msg.content.startsWith(prefix)) return;
 
-        //Get the Value of what is being rolled without brackets
-        const diceCountWithDiceSize = msg.content.substring(indexOfFirstBracket + 1, indexOfSecondBracket);
+    //Get indexes for brackets to be able to grab out the value provided
+    const indexOfFirstBracket = msg.content.indexOf('[');
+    const indexOfSecondBracket = msg.content.indexOf(']');
 
-        let numberOfDiceToRoll;
-        let dieSizeToRoll;
-        if (diceCountWithDiceSize.indexOf("d") === 0) {
-            //Rolling a single dice
-            numberOfDiceToRoll = 1;
-            dieSizeToRoll = diceCountWithDiceSize.substring(1)
-        }
-        else {
-            //Rolling multiple dice
-            dieSizeToRoll = diceCountWithDiceSize.substring(2);
-            numberOfDiceToRoll = diceCountWithDiceSize.substring(0, diceCountWithDiceSize.indexOf("d"));
-        }
+    //Get the Value of what is being rolled without brackets
+    const diceCountWithDiceSize = msg.content.substring(indexOfFirstBracket + 1, indexOfSecondBracket);
 
-        const valueRolled = Roll(numberOfDiceToRoll, dieSizeToRoll);
-        msg.reply(`Rolled a ${valueRolled}`)
+    let numberOfDiceToRoll;
+    let dieSizeToRoll;
+    if (diceCountWithDiceSize.indexOf("d") === 0) {
+        //Rolling a single dice
+        numberOfDiceToRoll = 1;
+        dieSizeToRoll = diceCountWithDiceSize.substring(1)
     }
+    else {
+        //Rolling multiple dice
+        dieSizeToRoll = diceCountWithDiceSize.substring(2);
+        numberOfDiceToRoll = diceCountWithDiceSize.substring(0, diceCountWithDiceSize.indexOf("d"));
+    }
+
+    const valueRolled = Roll(numberOfDiceToRoll, dieSizeToRoll);
+    msg.reply(`Rolled a ${valueRolled}`)
+
 });
 
 
