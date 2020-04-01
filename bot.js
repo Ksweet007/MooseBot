b7d71026db9ac8a62a260fbf350cdb7e3e624616
KyleSweetMeM <kyle.sweet@mem.com>
2020-04-01 10:35:30 -0400

    Changed back to env file for storing token

--- bot.js
+++ bot.js
@@ -1,12 +1,13 @@
+require('dotenv').config();
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix } = require('./config.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
 
 client.on('message', msg => {
     //If the message doesn't start with our prefix or a bot sent it, exit early
